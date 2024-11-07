cdf3dd78aed34470c0567974fef55b8aff0b775d
stephen-hui <huilt1232@gmail.com>
2024-11-07 17:09:47 -0500

    add user route,controller,model and auth flow

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+let usersController = require("../controllers/users");
+let authController = require("../controllers/auth");
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
+
+router.post("/signin", authController.signin);
+
+router.get("/list", usersController.list);
+router.post("/create", usersController.create);
+router.get("/get/:userID", usersController.userGet, usersController.userByID);
+router.put("/edit/:userID", usersController.update);
+
+// need to be signed in to delete
+router.delete(
+  "/delete/:userID",
+  authController.requireSignin,
+  usersController.remove
+);
 
 module.exports = router;
