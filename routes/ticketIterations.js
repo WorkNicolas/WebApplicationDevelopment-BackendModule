fafec90f7223968003fd94bbd3dd23bffdd5b18d
WorkNicolas <Carl.Nicolas.V.Mendoza@gmail.com>
2024-11-08 21:01:24 -0500

    Fixed ticketIteration using ticketID instead of ticketIterationID

--- routes/ticketIterations.js
+++ routes/ticketIterations.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const ticketIterationController = require('../controllers/ticketIterationController');
 router.post('/', ticketIterationController.create);
 router.get('/', ticketIterationController.list);
-router.get('/:ticketID', ticketIterationController.ticketGet, ticketIterationController.ticketByID);
+router.get('/:ticketID', ticketIterationController.ticketIterationGet, ticketIterationController.ticketIterationByID);
 router.put('/:ticketID', ticketIterationController.update);
 router.delete('/:ticketID', ticketIterationController.remove);
 
