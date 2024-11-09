96ca223d265a1d3ffa2752ee5f8bbcf6a424e1b9
WorkNicolas <Carl.Nicolas.V.Mendoza@gmail.com>
2024-11-08 20:56:37 -0500

    Modified routes to have the proper delete method name from .delete to .remove

--- routes/ticketIterations.js
+++ routes/ticketIterations.js
@@ -6,6 +6,6 @@ router.post('/', ticketIterationController.create);
 router.get('/', ticketIterationController.list);
 router.get('/:ticketID', ticketIterationController.ticketGet, ticketIterationController.ticketByID);
 router.put('/:ticketID', ticketIterationController.update);
-router.delete('/:ticketID', ticketIterationController.delete);
+router.delete('/:ticketID', ticketIterationController.remove);
 
 module.exports = router;
--- routes/tickets.js
+++ routes/tickets.js
@@ -6,6 +6,6 @@ router.post('/', ticketController.create);
 router.get('/', ticketController.list);
 router.get('/:ticketID', ticketController.ticketGet, ticketController.ticketByID);
 router.put('/:ticketID', ticketController.update);
-router.delete('/:ticketID', ticketController.delete);
+router.delete('/:ticketID', ticketController.remove);
 
 module.exports = router;
