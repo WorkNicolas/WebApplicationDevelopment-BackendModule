a007c1f03ea8adacff7fd77365c4370cdf4b1311
WorkNicolas <Carl.Nicolas.V.Mendoza@gmail.com>
2024-11-09 10:52:38 -0500

    ticketIteration now require signing in and admin privilege to use

--- routes/ticketIterations.js
+++ routes/ticketIterations.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const ticketIterationController = require('../controllers/ticketIterationController');
-const { requireSignin } = require('../controllers/authController');
-const { requireAdmin } = require('../middlewares/admin');
-router.post('/', requireSignin, requireAdmin, ticketIterationController.create);
-router.get('/', requireSignin, requireAdmin, ticketIterationController.list);
-router.get('/:ticketIterationID', requireSignin, requireAdmin, ticketIterationController.ticketIterationGet, ticketIterationController.ticketIterationById);
-router.put('/:ticketIterationID', ticketIterationController.update);
-router.delete('/:ticketIterationID', ticketIterationController.remove);
+const authController = require("../controllers/authController");
+router.post('/', authController.requireSignin, authController.requireSignin, ticketIterationController.create);
+router.get('/', authController.requireSignin, requireAdmin, ticketIterationController.list);
+router.get('/:ticketIterationID', authController.requireSignin, requireAdmin, ticketIterationController.ticketIterationGet, ticketIterationController.ticketIterationById);
+router.put('/:ticketIterationID', authController.requireSignin, authController.requireAdmin, ticketIterationController.update);
+router.delete('/:ticketIterationID', authController.requireSignin, authController.requireAdmin, ticketIterationController.remove);
 
 module.exports = router;
